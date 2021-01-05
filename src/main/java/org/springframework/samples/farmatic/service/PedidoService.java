0319126fca81d4edf4e7857c75a1fe0b68fa38f8
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-01-05 21:51:53 +0100

    Task-013: Modificar pedido como proveedor

--- src/main/java/org/springframework/samples/farmatic/service/PedidoService.java
+++ src/main/java/org/springframework/samples/farmatic/service/PedidoService.java
@@ -14,6 +14,7 @@ import org.springframework.samples.farmatic.model.Proveedor;
 import org.springframework.samples.farmatic.model.User;
 import org.springframework.samples.farmatic.repository.LineaPedidoRepository;
 import org.springframework.samples.farmatic.repository.PedidoRepository;
+import org.springframework.samples.farmatic.repository.ProductoRepository;
 import org.springframework.samples.farmatic.repository.ProveedorRepository;
 import org.springframework.samples.farmatic.repository.UserRepository;
 import org.springframework.security.core.Authentication;
@@ -23,26 +24,27 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PedidoService {
-	
-	private PedidoRepository pedidoRepository;
-		
-	private LineaPedidoRepository lineaRepository;
-		
-	private ProveedorRepository proveedorRepository;
-	
-	private ProductoRepository productoRepository;
-  
-  private UserRepository userRepository;
-	
+
+	private PedidoRepository		pedidoRepository;
+
+	private LineaPedidoRepository	lineaRepository;
+
+	private ProveedorRepository		proveedorRepository;
+
+	private ProductoRepository		productoRepository;
+
+	private UserRepository			userRepository;
+
+
 	@Autowired
-	public PedidoService(PedidoRepository pedidoRepository, LineaPedidoRepository lineaRepository, ProveedorRepository proveedorRepository, ProductoRepository productoRepository, UserRepository userRepository) {
+	public PedidoService(final PedidoRepository pedidoRepository, final LineaPedidoRepository lineaRepository, final ProveedorRepository proveedorRepository, final ProductoRepository productoRepository, final UserRepository userRepository) {
 		this.pedidoRepository = pedidoRepository;
 		this.lineaRepository = lineaRepository;
 		this.proveedorRepository = proveedorRepository;
 		this.productoRepository = productoRepository;
-    this.userRepository = userRepository;
+		this.userRepository = userRepository;
 	}
-	
+
 	//---------Metodos referente a PEDIDOS---------
 
 	@Transactional(readOnly = true)
@@ -69,62 +71,56 @@ public class PedidoService {
 	}
 
 	@Transactional
-	public void pedidoRecibido(Pedido pedido) throws DataAccessException{
-		pedido = pedido(pedido.getId());
+	public void pedidoRecibido(Pedido pedido) throws DataAccessException {
+		pedido = this.pedido(pedido.getId());
 		pedido.setEstadoPedido(EstadoPedido.Recibido);
 		pedido.setFechaEntrega(LocalDate.now());
-		for(LineaPedido linea:pedido.getLineaPedido()) {
+		for (LineaPedido linea : pedido.getLineaPedido()) {
 			Producto producto = linea.getProducto();
 			producto.sumaStock(linea.getCantidad());
-			productoRepository.save(producto);
+			this.productoRepository.save(producto);
 		}
-		pedidoRepository.save(pedido);
-  }
-
-  @Transactional
-	public Collection<LineaPedido> lineasPedido(final int id) throws DataAccessException {
-		//lineas del pedido
-		return this.lineaRepository.lineaPedido(id);
+		this.pedidoRepository.save(pedido);
 	}
 
 	@Transactional
-	public void enviarPedido(Proveedor provedor) throws DataAccessException{
-		Pedido pedido = pedidoActual();
+	public void enviarPedido(final Proveedor provedor) throws DataAccessException {
+		Pedido pedido = this.pedidoActual();
 		pedido.setProveedor(provedor);
 		pedido.setEstadoPedido(EstadoPedido.Pedido);
 		pedido.setFechaPedido(LocalDate.now());
-		pedidoRepository.save(pedido);
+		this.pedidoRepository.save(pedido);
 		Pedido nuevoPedido = new Pedido();
-		nuevoPedido.setCodigo(getCodigoPedidoNuevo(pedido.getCodigo()));
-		pedidoRepository.save(nuevoPedido);
+		nuevoPedido.setCodigo(this.getCodigoPedidoNuevo(pedido.getCodigo()));
+		this.pedidoRepository.save(nuevoPedido);
 	}
-	
-	private String getCodigoPedidoNuevo(String codigo) {
+
+	private String getCodigoPedidoNuevo(final String codigo) {
 		String res = "P-";
 		String[] aux = codigo.split("P-");
 		Integer num = Integer.parseInt(aux[1]);
 		int n = num++, cifras = 0;
-		while(n!=0) {
-			n = n/10;
+		while (n != 0) {
+			n = n / 10;
 			cifras++;
 		}
-		if(cifras == 1) {
+		if (cifras == 1) {
 			res += "00" + num.toString();
-		}else if(cifras == 2) {
+		} else if (cifras == 2) {
 			res += "0" + num.toString();
-		}else {
+		} else {
 			res += num.toString();
 		}
 		return res;
 	}
-	
+
 	//---------Metodos referente a LINEAS DE PEDIDOS---------
-	
+
 	@Transactional
-	public Collection<LineaPedido> lineasPedido(int id) throws DataAccessException{
+	public Collection<LineaPedido> lineasPedido(final int id) throws DataAccessException {
 		//lineas del pedido
-		return lineaRepository.lineaPedido(id);
-  }
+		return this.lineaRepository.lineaPedido(id);
+	}
 
 	@Transactional
 	public void saveLinea(final LineaPedido linea) throws DataAccessException {
@@ -133,7 +129,7 @@ public class PedidoService {
 	}
 
 	@Transactional
-	public LineaPedido newLinea(Producto producto, Integer cantidad) throws DataAccessException{
+	public LineaPedido newLinea(final Producto producto, final Integer cantidad) throws DataAccessException {
 		//creando linea de pedido vacia
 		Pedido pedido = this.pedidoActual();
 		LineaPedido linea = new LineaPedido();
@@ -142,19 +138,19 @@ public class PedidoService {
 		linea.setCantidad(cantidad);
 		return linea;
 	}
-	
+
 	//---------Metodos referente a PROVEEDOR---------
-	
+
 	@Transactional
-	public Proveedor proveedor(int id) {
-		return proveedorRepository.findById(id);
+	public Proveedor proveedor(final int id) {
+		return this.proveedorRepository.findById(id);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Collection<Proveedor> findProveedores() {
-		return (Collection<Proveedor>) proveedorRepository.findAll();
-  }
-  
+		return (Collection<Proveedor>) this.proveedorRepository.findAll();
+	}
+
 	private User getCurrentUser() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String currentPrincipalName = authentication.getName();             //Obtiene el nombre del ususario actual
@@ -169,10 +165,9 @@ public class PedidoService {
 	}
 
 	@Transactional
-	public Pedido proveedorEnviarPedido(final Pedido pedido) {
-		if (pedido.getEstadoPedido() == EstadoPedido.Borrador) {
-			pedido.setEstadoPedido(EstadoPedido.Enviado);
-		}
-		return pedido;
+	public void pedidoEnviado(Pedido pedido) throws DataAccessException {
+		pedido = this.pedido(pedido.getId());
+		pedido.setEstadoPedido(EstadoPedido.Enviado);
+		this.pedidoRepository.save(pedido);
 	}
 }
