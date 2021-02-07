b80eca8d8b6a1d2d0cca5034473986c4d4333794
antrosbar <antrosbar.profesional@gmail.com>
2021-02-07 18:01:44 +0100

    Test unitario de mostrar ventas de cliente añadido

--- src/test/java/org/springframework/samples/farmatic/service/VentaServiceTests.java
+++ src/test/java/org/springframework/samples/farmatic/service/VentaServiceTests.java
@@ -1,13 +1,28 @@
 package org.springframework.samples.farmatic.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.farmatic.model.Cliente;
+import org.springframework.samples.farmatic.model.Producto;
+import org.springframework.samples.farmatic.model.User;
+import org.springframework.samples.farmatic.model.Venta;
+import org.springframework.samples.farmatic.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class VentaServiceTests {
+	
+	
 	
 }
