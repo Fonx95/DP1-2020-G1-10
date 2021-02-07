6a8ca55282fc247248f2e214e26c01a4b647cecb
fernando <fernando@localhost>
2021-02-07 18:53:46 +0100

    Test Cliente Service

--- src/test/java/org/springframework/samples/farmatic/service/ClienteServiceTests.java
+++ src/test/java/org/springframework/samples/farmatic/service/ClienteServiceTests.java
@@ -1,13 +1,97 @@
 package org.springframework.samples.farmatic.service;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
+
+import org.assertj.core.api.Assertions;
+ 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.farmatic.model.Authorities;
+import org.springframework.samples.farmatic.model.Cliente;
+import org.springframework.samples.farmatic.model.Farmaceutico;
+import org.springframework.samples.farmatic.model.User;
+import org.springframework.samples.farmatic.repository.ClienteRepository;
+import org.springframework.samples.farmatic.repository.FarmaceuticoRepository;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class ClienteServiceTests {
 	
+	@Autowired
+	protected ClienteService clienteService;
+	@Autowired
+	protected ClienteRepository clienteRepository;
+
+	//Test positivos
+	
+	@Test
+	void shouldFindById() {
+		Assertions.assertThat(this.clienteService.findClienteById(1)).isNotNull();
+	}
+	
+	@Test
+	void shouldFindAll() {
+		Assertions.assertThat(this.clienteService.findClientes()).isNotEmpty();
+	}
+
+	
+	@Test
+	public void saveCliente(){
+
+		Cliente cliente = new Cliente();
+		cliente.setProvincia("Sevilla");
+		cliente.setLocalidad("Alcala");
+		cliente.setDireccion("Calle Pinto");
+		cliente.setPorPagarTotal(35.0);
+		User user= new User();
+		user.setCliente(cliente);
+		user.setPassword("1234");
+		user.setUsername("cliente1");
+		cliente.setDni("20035098Y");
+		cliente.setSurnames("Maria Polo");
+		
+		
+
+		this.clienteService.saveCliente(cliente);
+		Cliente client1=this.clienteService.clienteDni("20035098Y");
+		Assertions.assertThat(this.clienteService.clienteDni("20035098Y")).isNotNull();
+		
+	}
+	//Test Negativos
+	@Test
+	void shouldNotFindById() {
+		Assertions.assertThat(this.clienteService.findClienteById(-1)).isNull();
+	}
+
+	@Test
+	void notSaveCliente(){
+
+		Cliente cliente = new Cliente();
+		
+		cliente.setProvincia("Sevilla");
+		cliente.setLocalidad("Alcala");
+		cliente.setDireccion("Calle Pinto");
+		cliente.setPorPagarTotal(35.0);
+		User user= new User();
+		user.setCliente(cliente);
+		user.setPassword("1234");
+		user.setUsername("cliente1");
+		cliente.setDni("20035098Y");
+		cliente.setSurnames("");
+		
+
+		//this.clienteService.saveCliente(cliente);
+		assertThrows(ConstraintViolationException.class, ()->{this.clienteService.saveCliente(cliente);});
+		//assertThrows(ConstraintViolationException.class, ()->{this.clienteService.clienteDni("20035098Y").getClass();});
+		
+	}
 }
