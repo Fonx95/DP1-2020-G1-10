032d984cfd132105d4e1878413a8a2205d1cc4ef
antrosbar <antrosbar.profesional@gmail.com>
2021-02-09 18:56:52 +0100

    Añadidos test de producto

--- src/test/java/org/springframework/samples/farmatic/service/ProductoServiceTests.java
+++ src/test/java/org/springframework/samples/farmatic/service/ProductoServiceTests.java
@@ -4,10 +4,16 @@ package org.springframework.samples.farmatic.service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.assertj.core.util.Lists;
+import org.hibernate.TransientObjectException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.farmatic.model.Producto;
+import org.springframework.samples.farmatic.model.TipoMedicamento;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,11 +44,35 @@ class ProductoServiceTests {
 		aux.iterator().forEachRemaining(x -> lista.add(x));
 		assertThat(lista.size()).isEqualTo(13); // Comprobará que el número de elementos de la lista sea correcto. Depende de la base de datos.
 	}
+	
+	@Test
+	@Transactional
+	public void shouldFindTipoMedicamentos() {
+		
+		
+		Collection<TipoMedicamento> tm = this.productoService.getMedicamentoTypes();
+		
+		assertThat(tm.size()==12);
+		
+	}
 
 	@Test
 	void shouldFindProductoByID() {
 		Producto p = this.productoService.findProductoById(1);
 		assertThat(p.getCode().equals("PR-001"));
+	}
+	
+	@Test
+	void shouldFindProductoByCode() {
+		Producto p = this.productoService.findProductoByCode("PR-001");
+		assertThat(p.getName().equals("ASPIRINA"));
+	}
+	
+	@Test
+	void shouldFindProductoByName() {
+		String s = "PIR";
+		Collection<Producto> ps = this.productoService.productoPorNombre(s);
+		assertThat(ps.stream().allMatch(p->p.getName().toLowerCase().contains(s.toLowerCase())));
 	}
 	
 	@Test
@@ -85,25 +116,54 @@ class ProductoServiceTests {
 		
 	}
 	
+	
 	@Test
 	@Transactional
-	public void shouldNotModifyProduct() {
+	public void shouldFindProductByTipo() {
+		
+		
+		Collection<TipoMedicamento> tm = this.productoService.getMedicamentoTypes();
+		List<TipoMedicamento> types = Lists.newArrayList(tm);
 
-		Producto producto = this.productoService.findProductoByCode("PR-001");
-		producto.setPvp(0.0);
-                
-		this.productoService.saveProducto(producto);
+		Collection<Producto> productos = this.productoService.findProductosByTipo(types.get(0));
 		
 		
-
-		
+		assertThat(productos.stream().allMatch(p->p.getTipoMedicamento().contains(types.get(0))));
 		
 	}
 	
 	//Test negativos
 	
 	@Test
+	@Transactional
+	public void shouldNotFindProductByTipo() {
+		
+		
+		TipoMedicamento tm = new TipoMedicamento();
+		List<TipoMedicamento> types = Lists.newArrayList(tm);
+
+
+		
+		assertThrows(Exception.class, ()->{this.productoService.findProductosByTipo(types.get(0));});
+		
+		
+	}
+	
+	
+	@Test
 	void shouldNotFindProductoByID() {
 		assertThrows(NullPointerException.class, ()->{this.productoService.findProductoById(0).getClass();}); // Comprobará que se lanza un NullPointerException al realizar la acción en el corchete.
+	}
+	
+	@Test
+	void shouldNotFindProductoByName() {
+		String s = "dafhsdgjgahadfhadfhdafhdfhadhadfhfad";
+		assertThrows(EntityNotFoundException.class, ()->{this.productoService.productoPorNombre(s);});
+
+	}
+	
+	@Test
+	void shouldNotFindProductoByCode() {
+		assertThrows(EntityNotFoundException.class, ()->{this.productoService.findProductoByCode("dadfhsdhafdhafdhagsdadfh");});
 	}
 }
